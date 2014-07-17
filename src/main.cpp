656509ed18199fb475261b523095ea780adc3c8c
edn247 <edn247@gmail.com>
2014-07-16 22:46:26 -0700

    PoW reward cut to fees only
    This should be ok for an attempt to get to chain stake moving - not a good plan for long term PoW strategy

--- src/main.cpp
+++ src/main.cpp
@@ -964,35 +964,15 @@ uint256 WantedByOrphan(const CBlock* pblockOrphan)
 // miner's coin base reward
 int64_t GetProofOfWorkReward(int64_t nFees)
 {
-    int64_t nSubsidy = 10 * COIN;
-	if(pindexBest->nHeight < 3)
-    {
-        nSubsidy = 1000 * COIN;
-    }
-		else if(pindexBest->nHeight < 200)
-    {
-		nSubsidy = 5 * COIN;
-    }
-		else if(pindexBest->nHeight < 1000)
-    {
-		nSubsidy = 10 * COIN;
-    }
-		else if(pindexBest->nHeight < 2500)
-    {
-		nSubsidy = 100 * COIN;
-    }
-		else if(pindexBest->nHeight < 5000)
-    {
-		nSubsidy = 10 * COIN;
-    }
-	
+    int64_t nSubsidy = 0 * COIN;
+
     if (fDebug && GetBoolArg("-printcreation"))
         printf("GetProofOfWorkReward() : create=%s nSubsidy=%"PRId64"\n", FormatMoney(nSubsidy).c_str(), nSubsidy);
 	
     return nSubsidy + nFees;
 }
 
-const int DAILY_BLOCKCOUNT =  2880;
+const int DAILY_BLOCKCOUNT =  1440;
 // miner's coin stake reward based on coin age spent (coin-days)
 int64_t GetProofOfStakeReward(int64_t nCoinAge, int64_t nFees)
 {
