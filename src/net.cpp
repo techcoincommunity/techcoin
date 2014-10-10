00b12298d6c7194dbf743f588806c7fc5f2ee057
techcoincommunity <techcoincommunity@gmail.com>
2014-10-10 12:49:19 -0400

    New seed node

--- src/net.cpp
+++ src/net.cpp
@@ -1139,7 +1139,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    {"chainworks", "tech.chainworks.info"} //Main Techcoin Seed Nodes
+    {"54.69.85.176", "54.69.85.176"} //Main Techcoin Seed Node
 };
 void ThreadDNSAddressSeed(void* parg)
 {
